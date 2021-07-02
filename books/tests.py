d05604b3e9a693f3f50f64bb98705ae04367be12
sophie200 <44643926+sophie200@users.noreply.github.com>
2021-07-01 23:43:31 -0500

    added query

--- books/tests.py
+++ books/tests.py
@@ -7,6 +7,7 @@ class ReporterTests(TestCase):
 
     def setUp(self):
         self.author = Author.objects.create(first_name = 'Bob', last_name = 'Ross')
+        self.author = Author.objects.create(first_name = 'Bob')
     
     def test_reporter(self):
         self.assertEqual(f'{self.first_name}', 'Bob')
